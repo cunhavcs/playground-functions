7cec0d61b0e9bf7a7c7906c0430962b0fc6657b3
cunhavcs <cunha.vcs@gmail.com>
2022-12-19 18:15:27 -0300

    Refatora a função generatePhoneNumber (Desafio 11)

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,15 +1,15 @@
 // Desafio 11
-function generatePhoneNumber(numbers) {
-  let phoneNumber = "";
+const generatePhoneNumber = (numbers) => {
+  let phoneNumber = '';
   let currentCount = 0;
 
-  if (numbers.length !== 11){
-    return "Array com tamanho incorreto.";
+  if (numbers.length !== 11) {
+    return 'Array com tamanho incorreto.';
   }
 
   for (let index = 0; index < numbers.length; index += 1) {
     if (numbers[index] < 0 || numbers[index] > 9) {
-      return "não é possível gerar um número de telefone com esses valores";
+      return 'não é possível gerar um número de telefone com esses valores';
     }
 
     for (let index2 = 0; index2 < numbers.length; index2 += 1) {
@@ -19,26 +19,26 @@ function generatePhoneNumber(numbers) {
     }
 
     if (currentCount >= 3) {
-      return "não é possível gerar um número de telefone com esses valores";
+      return 'não é possível gerar um número de telefone com esses valores';
     }
     currentCount = 0;  
   
     switch (index) {
       case 0:
-        phoneNumber += "(" + numbers[index];
+        phoneNumber += `(${numbers[index]}`;
         break;
       case 2:
-        phoneNumber += ") " + numbers[index];
+        phoneNumber += `) ${numbers[index]}`;
         break;
       case 7:
-        phoneNumber += "-" + numbers[index];
+        phoneNumber += `-${numbers[index]}`;
         break;
       default:
         phoneNumber += numbers[index];
     }
   }
   return phoneNumber;
-}
+};
 
 // Desafio 12
 function triangleCheck(lineA, lineB, lineC) {
